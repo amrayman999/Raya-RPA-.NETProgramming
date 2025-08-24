a44e665847a497054dcbdeef1307c1cc53ce77c1
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-08-24 15:09:00 +0300

    Assignment01 Project Uploaded

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string str = "The ##enchanted## forest whispered secrets to the ##daring## young explorer, " +
+                "Amelia. As she ventured deeper, the sunlight struggled to pierce the thick canopy of ancient trees. Strange ##creatures##" +
+                " rustled in the undergrowth, their ##whispers## echoing through the air. A sense of ##wonder## filled Amelia, a mix of fear and" +
+                " excitement. Suddenly, a glint of light caught her eye. A small, ##crystalline## stream flowed nearby, its water sparkling like diamonds. " +
+                "As Amelia knelt to drink, a voice boomed from behind, 'Welcome, " +
+                "traveler, to the heart of the ##enigmatic## forest!'";
+            // removing ##
+            str = str.Replace("##", "");
+
+            // splitting the string into words
+            string[] words = str.Split(" ");
+            Console.WriteLine();
+            Console.WriteLine($"Character Name: {words[10]}");
+
+            //to uppercase
+            Console.WriteLine(str.ToUpper());
+            Console.WriteLine();
+            //to lowercase
+            Console.WriteLine(str.ToLower());
         }
     }
 }
